0fc887ebcc71583643a76eedeffcf6a87eb29438
dinostheo <dinostheo@gmail.com>
2013-10-19 14:41:02 +0200

    Generate a gif from the captures

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.post('/upload', upload.upload);
+app.post('/gif', upload.gif);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var im = require('imagemagick');
+var rimraf = require('rimraf');
 
 exports.upload = function(req, res) {
     var message = false;
@@ -9,7 +10,7 @@ exports.upload = function(req, res) {
     var thumb_filename = 'thumb_' + img_filename;
     var img_path = upload_dir + img_filename;
     var thumb_path = upload_dir + thumb_filename;
-    
+
     fs.writeFile(img_path, new Buffer(req.body.bufferedImage, "base64"), function(err) {
       im.resize({
         srcData: fs.readFileSync(img_path, 'binary'),
@@ -21,3 +22,35 @@ exports.upload = function(req, res) {
       });
     });
 };
+
+exports.gif = function(req, res) {
+    var thumbnails = req.body.frameBuffer;
+
+    fs.mkdir(__dirname + '/../public/uploads/tmp/', function(){
+        console.log('Dir tmp created');
+    });
+
+    thumbnails.forEach(function(value){
+        var img_part = value.split('thumb_')[1];
+
+        fs.readFile(__dirname + '/../public/uploads/' + img_part, function(err, data) {
+            if (err) throw err;
+            fs.writeFile(__dirname + '/../public/uploads/tmp/' + img_part, data, function (err) {
+                if (err) throw err;
+            });
+        });
+    });
+
+    var gif_filename = 'gif_' + new Date().getTime() + '.gif';
+
+    im.convert(['-delay', '30', '-loop', '0', __dirname + '/../public/uploads/tmp/img_*.png', __dirname + '/../public/uploads/gifs/' + gif_filename], 
+    function(err, stdout){
+        if (err) throw err;
+        console.log('GIF is generated');
+        
+        //Remove the tmp dir
+        rimraf(__dirname + '/../public/uploads/tmp/', function(err){
+            if (err) throw new Error("ERROR!!!")
+        });
+    });
+}
