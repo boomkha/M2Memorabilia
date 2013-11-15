bfd27d5df4f24646734cdfcf6ef4fc938a9152e5
Leonidas Diamantis <leonidas@m2mobi.com>
2013-11-15 17:39:52 +0100

    Just a bit of fancyness in the gallery

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,15 +1,20 @@
-function play_pause_gif(e) {
+function play_pause_gif(ele) {
+    var e = $(ele).children('img');
+    var s = $(ele).children('span');
     var id = $(e).attr('id');
     console.log(e);
     var gif_src = '/gifs/' + $('#' + id + 'gif').val();
     var origin_src = '/uploads/' + $('#' + id + 'origin').val();
-    if ($(e).attr('class') == 'paused') {
+
+    if ($(e).hasClass('paused')) {
         $(e).removeClass('paused');
         $(e).addClass('playing');
         $(e).attr('src', gif_src);
+        $(s).hide();
     } else {
         $(e).removeClass('playing');
         $(e).addClass('paused');
         $(e).attr('src', origin_src);
+        $(s).show();
     }
 }
